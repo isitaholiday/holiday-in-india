c09a74184660b93c64092491cc0d847943c5dfd3
isitaholiday <alitanveerx@gmail.com>
2024-12-02 16:07:15 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
     const holidayStatus = document.getElementById('holiday-status');
     const currentDate = new Date();
     const holidays = [
-        '2023-01-01', // New Year's Day
-        '2023-01-26', // Australia Day
-        '2023-04-07', // Good Friday
-        '2023-04-10', // Easter Monday
-        '2023-04-25', // Anzac Day
-        '2023-06-12', // Queen's Birthday (most states)
-        '2023-10-02', // Labour Day (most states)
-        '2023-12-02', // Christmas Day
-        '2023-12-26'  // Boxing Day
+        '2024-01-01', // New Year's Day
+        '2024-01-26', // Australia Day
+        '2024-04-07', // Good Friday
+        '2024-04-10', // Easter Monday
+        '2024-04-25', // Anzac Day
+        '2024-06-12', // Queen's Birthday (most states)
+        '2024-10-02', // Labour Day (most states)
+        '2024-12-02', // Christmas Day
+        '2024-12-26'  // Boxing Day
     ];
 
     const today = currentDate.toISOString().split('T')[0];
